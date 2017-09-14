bfd00d16fc7610e5055bf376b03a1692f4fd8f4f
Jackson Chegenye <chegenyejackson@gmail.com>
2017-09-14 16:50:29 +0100

    Authentication Modules In General - Based on Registration, Login, ResetPassword, ForgotPassword

--- config/database.php
+++ config/database.php
@@ -43,15 +43,23 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'laravel5modules'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+        
+            /*'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',*/
+
+            //Fix that says Syntax error or access violation: 1071 Specified key was too long
+            //This is for using wamp and using phpmyadmin to access mysql
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+
             'prefix' => '',
             'strict' => true,
             'engine' => null,
+
         ],
 
         'pgsql' => [
